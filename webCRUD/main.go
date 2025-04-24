0c9a0d2cc0ff93bc3a63228c4ebaa4f57d552d14
arthur <arthurazevedods@gmail.com>
2025-04-24 10:00:42 -0300

    server with routing

--- webCRUD/main.go
+++ webCRUD/main.go
@@ -10,6 +10,10 @@ func main() {
 		fmt.Fprintf(w, "Hello, World!")
 	})
 
+	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to the Users page!")
+	})
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println("Server error:", err)
 	}
